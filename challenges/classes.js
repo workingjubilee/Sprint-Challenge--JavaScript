b5ad33bbb1ed018e99cc8e9f10a443071e7a09f7
workingjubilee <workingjubilee@gmail.com>
2019-01-25 10:23:39 -0800

    Classes stretch met.

--- challenges/classes.js
+++ challenges/classes.js
@@ -26,3 +26,19 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+  constructor(edge) {
+    super(edge,edge,edge);
+}
+}
+
+const smalls = new CubeMaker(2);
+console.log(smalls.volume());
+// The formula is a^3, where a = one edge dimension (since HLW are identical).
+// so, 2^3 = 8, which is the output!
+// no change required, in other words.
+console.log(smalls.surfaceArea());
+// The formula is 6a^2, where a = one edge dimension (since HLW are identical).
+// so, 6*2^2 = 24, which is the output!
+// no change required, in other words.
---
+++ challenges/objectstestnew.js
@@ -0,0 +1,1 @@
+Was an empty file created to "start over" on objects-arrays.js from a previous commit, is now objects-arrays.js
