cdafc0ec725bbe4927672ffb164d3030118ec24b
workingjubilee <workingjubilee@gmail.com>
2019-01-25 08:32:12 -0800

    Done with functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,7 +8,10 @@
 */
 
 function consume(a,b,cb) {
-  return cb (a,b);
+  // Had to test to make sure this was working.
+  // console.log('Consume worked?')
+  // console.log('Consume things a is: ' + a);
+  return cb(a,b);
 }
 
 
@@ -18,11 +21,24 @@ function consume(a,b,cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(x,y){
+  return x + y;
+}
+
+function multiply(x,y){
+  return x * y;
+}
+
+function greeting(firstName,lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+// Had to edit these to use console.log because I test by running node
+// node wasn't displaying non-logged return values.
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ====
@@ -30,6 +46,8 @@ function consume(a,b,cb) {
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation:
+
+// Within the global environment, "external" is accessible to everything else I could put in the global environment, including myFunction. Inside "myFunction", everything is accessible, in the same way, to anything inside myFunction -- it is a world within a world.
 
 
 const external = "I'm outside the function";
